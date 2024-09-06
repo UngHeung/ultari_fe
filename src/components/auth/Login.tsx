215f666af3bbb5c7569f35fabf2f7378b911d4de
ungheung <t01047377477@gmail.com>
2024-09-07 00:48:17 +0900

    fix : signup -> login 코드 변경

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -4,55 +4,41 @@ import BaseButton from '../common/BaseButton';
 import style from './styles/button.module.css';
 
 const Login = () => {
-  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+  const handleLogin = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     const formData = new FormData(event.currentTarget);
     const data = {
       account: formData.get('account'),
       password: formData.get('password'),
-      phone: formData.get('phone'),
-      email: formData.get('email'),
     };
 
     console.log(data);
   };
 
-  const handleSignUp = (event: MouseEvent<HTMLButtonElement>) => {
+  const handleButton = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    console.log('회원가입');
+
+    console.log('회원가입으로 이동');
   };
 
   return (
     <form onSubmit={handleLogin}>
       <AuthInput
         name={'account'}
-        id={'auth_account'}
+        id={'login_account'}
         type={'text'}
         labelValue={'아이디'}
       />
       <AuthInput
         name={'password'}
-        id={'auth_password'}
+        id={'login_password'}
         type={'password'}
         labelValue={'비밀번호'}
-      />
-      <AuthInput
-        name={'phone'}
-        id={'auth_phone'}
-        type={'text'}
-        labelValue={'핸드폰번호'}
-      />
-      <AuthInput
-        name={'email'}
-        id={'auth_email'}
-        type={'email'}
-        labelValue={'이메일'}
       />
 
       <div className={style.buttonWrap}>
         <BaseButton type={'submit'} value={'로그인'} />
-        <BaseButton type={'button'} value={'회원가입'} onClick={handleSignUp} />
+        <BaseButton type={'button'} value={'회원가입'} onClick={handleButton} />
       </div>
     </form>
   );
