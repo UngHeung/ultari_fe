ab7c54d523251954bf5503cc50c4748db7506900
ungheung <t01047377477@gmail.com>
2024-09-07 00:52:40 +0900

    feat : 로고 크기 스케일 값 받아서 변경하는 기능

--- src/components/common/functions/generateLogoImgOptions.ts
+++ src/components/common/functions/generateLogoImgOptions.ts
@@ -2,11 +2,14 @@ import basicLogo from '@/public/images/logo_basic.png';
 import smallLogo from '@/public/images/logo_small.png';
 import style from '../styles/logo.module.css';
 
-export const generateLogoImgOptions = (type: 'basic' | 'small') => {
+export const generateLogoImgOptions = (
+  type: 'basic' | 'small',
+  scale: number,
+) => {
   return {
     src: type === 'basic' ? basicLogo : smallLogo,
     className: type === 'basic' ? style.basicLogo : style.smallLogo,
-    width: type === 'basic' ? 250 : 150,
-    height: 150,
+    width: type === 'basic' ? 250 * (scale / 100) : 150 * (scale / 100),
+    height: 150 * (scale / 100),
   };
 };
