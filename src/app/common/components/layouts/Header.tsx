16e5c013f839e627f1e0f0a3fc46f45dc63eb13d
ungheung <t01047377477@gmail.com>
2024-09-06 14:51:52 +0900

    feat : header 컴포넌트

--- src/app/common/components/layouts/Header.tsx
+++ src/app/common/components/layouts/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <nav>
+    <ul>
+      <li>
+        <Link href={'/'}>home</Link>
+      </li>
+      <li>
+        <Link href={'/auth'}>auth</Link>
+      </li>
+      <li>
+        <Link href={'/post'}>post</Link>
+      </li>
+    </ul>
+  </nav>;
+};
 
-export default Header
+export default Header;
