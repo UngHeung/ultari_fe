cac26f3016ba0e1fec604625d80a72296a67868a
ungheung <t01047377477@gmail.com>
2024-09-07 00:59:49 +0900

    feat : forgot, sign, post 뼈대 추가

---
+++ src/app/(auth)/forgot/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const forgot = () => {
+  return <div>forgot</div>;
+};
+
+export default forgot;
---
+++ src/app/post/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>post page</div>;
+};
+
+export default page;
