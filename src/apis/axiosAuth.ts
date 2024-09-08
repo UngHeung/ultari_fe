97d0454646f8f5858d1eda99f9ca7aa83cd71c78
ungheung <t01047377477@gmail.com>
2024-09-08 22:51:24 +0900

    fix : 코드 미동작 수정

--- src/apis/axiosAuth.ts
+++ src/apis/axiosAuth.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { BASE_URL } from '@/components/common/constants/pathConst';
+import { ACCESS_TOKEN } from '@/components/auth/constants/accessToken';
 import { reissueAccessToken } from './functions/reissueToken';
-import { requestInterceptors } from './functions/requestInterceptors';
-import { responseInterceptors } from './functions/responseInterceptors';
 
 export const authAxios = axios.create({
   baseURL: BASE_URL,
@@ -12,7 +11,37 @@ export const authAxios = axios.create({
   },
 });
 
-(async () => {
-  await requestInterceptors(authAxios);
-  await responseInterceptors(authAxios);
-})();
+authAxios.interceptors.request.use(
+  config => {
+    const accessToken = ACCESS_TOKEN.accessToken;
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+authAxios.interceptors.response.use(
+  response => {
+    if (response.status === 404) {
+      console.log('404 Page');
+    }
+
+    return response;
+  },
+  async error => {
+    if (error.response?.status === 401) {
+      // isTokenExpired()
+      ACCESS_TOKEN.accessToken = await reissueAccessToken();
+      // isTokenExpired() && await reissueToken();
+      error.config.headers = {
+        Authorization: `Bearer ${ACCESS_TOKEN.accessToken}`,
+      };
+
+      const response = await axios.request(error.config);
+      return response;
+    }
+    return Promise.reject(error);
+  },
+);
