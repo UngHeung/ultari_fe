08b28d080a019870212dd1cb925dd4fadd461fee
ungheung <t01047377477@gmail.com>
2024-09-08 22:58:33 +0900

    update : 로그아웃 path 추가

--- src/components/common/constants/pathConst.ts
+++ src/components/common/constants/pathConst.ts
@@ -4,3 +4,4 @@ export const REISSUE_ACCESS = 'auth/reissue/access';
 export const REISSUE_REFRESH = 'auth/reissue/refresh';
 export const LOGIN_PATH = 'auth/login/account';
 export const SIGN_UP_PATH = 'auth/signup';
+export const LOGOUT_PATH = 'auth/logout';
