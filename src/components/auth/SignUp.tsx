0d5aa62998c6a2bb310800d4916756760f92db3d
ungheung <t01047377477@gmail.com>
2024-09-08 22:56:14 +0900

    refactor : 회원가입 성공시 로그인페이지로 이동, 가입 요청하는 동안 버튼 비활성화

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -3,69 +3,85 @@ import BaseButton from '../common/BaseButton';
 import style from './styles/button.module.css';
 import { useRouter } from 'next/navigation';
 import { handleSignUp } from './handlers/hendleSignUp';
+import { useState } from 'react';
 
 const SignUp = () => {
   const router = useRouter();
+  const [disabled, setDisabled] = useState<boolean>(true);
 
   return (
-    <form onSubmit={handleSignUp}>
-      <AuthInput
-        name={'account'}
-        id={'auth_account'}
-        type={'text'}
-        labelValue={'아이디'}
-        placeholder={' '}
-      />
-      <AuthInput
-        name={'password'}
-        id={'auth_password'}
-        type={'password'}
-        labelValue={'비밀번호'}
-        placeholder={' '}
-      />
-      <AuthInput
-        name={'password_check'}
-        id={'auth_password_check'}
-        type={'password'}
-        labelValue={'비밀번호 확인'}
-        placeholder={' '}
-      />
-      <AuthInput
-        name={'name'}
-        id={'auth_name'}
-        type={'text'}
-        labelValue={'이름'}
-        placeholder={' '}
-      />
-      <AuthInput
-        name={'phone'}
-        id={'auth_phone'}
-        type={'text'}
-        labelValue={'핸드폰번호'}
-        placeholder={' '}
-      />
-      <AuthInput
-        name={'email'}
-        id={'auth_email'}
-        type={'email'}
-        labelValue={'이메일'}
-        placeholder={' '}
-      />
+    <>
+      <form
+        onSubmit={async event => {
+          event.preventDefault();
 
-      <div className={style.buttonWrap}>
-        <BaseButton
-          styleClass={style.authButton}
-          type={'submit'}
-          value={'가입하기'}
+          setDisabled(true);
+          const { success, message } = await handleSignUp(event);
+
+          success && router.replace('/login');
+          setDisabled(false);
+        }}
+      >
+        <AuthInput
+          name={'account'}
+          id={'auth_account'}
+          type={'text'}
+          labelValue={'아이디'}
+          placeholder={' '}
         />
-        <BaseButton
-          styleClass={style.authButton}
-          type={'button'}
-          value={'취소'}
-          onClick={() => router.back()}
+        <AuthInput
+          name={'password'}
+          id={'auth_password'}
+          type={'password'}
+          labelValue={'비밀번호'}
+          placeholder={' '}
         />
-      </div>
-    </form>
+        <AuthInput
+          name={'password_check'}
+          id={'auth_password_check'}
+          type={'password'}
+          labelValue={'비밀번호 확인'}
+          placeholder={' '}
+        />
+        <AuthInput
+          name={'name'}
+          id={'auth_name'}
+          type={'text'}
+          labelValue={'이름'}
+          placeholder={' '}
+        />
+        <AuthInput
+          name={'phone'}
+          id={'auth_phone'}
+          type={'text'}
+          labelValue={'핸드폰번호'}
+          placeholder={' '}
+        />
+        <AuthInput
+          name={'email'}
+          id={'auth_email'}
+          type={'email'}
+          labelValue={'이메일'}
+          placeholder={' '}
+        />
+
+        <div className={style.buttonWrap}>
+          <BaseButton
+            styleClass={style.authButton}
+            type={'submit'}
+            value={'가입하기'}
+            disabled={disabled}
+          />
+          <BaseButton
+            styleClass={style.authButton}
+            type={'button'}
+            value={'취소'}
+            disabled={disabled}
+            onClick={() => router.back()}
+          />
+        </div>
+      </form>
+    </>
   );
 };
 
