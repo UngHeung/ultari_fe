902d58b9154b55fcc7b19f51b82b9ad94275b6e4
ungheung <t01047377477@gmail.com>
2024-09-09 20:32:14 +0900

    refactor : 모달 상태를 rtk로 관리하도록 변경

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,32 +1,24 @@
 'use client';
 
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './styles/modal.module.css';
 import Image from 'next/image';
 import modalSuccess from '@/public/images/modal_success.png';
 import modalCloseButton from '@/public/images/modal_close_button.png';
 import { PUBLIC_IMAGE_PATH } from '../common/constants/pathConst';
 import { useRouter } from 'next/navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetModal } from '../stores/reducer/modalRducer';
+import { SliceOptions } from '../stores/constants/stateOptions';
+import { modalType } from './constants/modalConst';
 
-const Modal = ({
-  title,
-  type,
-  success,
-  message,
-  path,
-  modalIsShow,
-  setModalIsShow,
-}: {
-  title: string;
-  type: 'alert' | 'confirm' | 'prompt';
-  success?: boolean;
-  message: string;
-  path?: string;
-  modalIsShow: boolean;
-  setModalIsShow: Dispatch<SetStateAction<boolean>>;
-}) => {
+const Modal = () => {
   const router = useRouter();
   const [theme, setTheme] = useState<'dark' | 'light'>('dark');
+  const { title, type, success, message, path } = useSelector(
+    (state: SliceOptions) => state?.modal,
+  );
+  const dispatch = useDispatch();
 
   useEffect(() => {
     window
@@ -42,15 +34,13 @@ const Modal = ({
 
   return (
     <>
-      <div
-        className={`${style.modalCover} ${modalIsShow ? style.isShow : style.isNotShow}`}
-      >
+      <div className={`${style.modalCover}`}>
         <article className={style.modalWrap}>
           <header className={style.modalHeader}>
             <h2 className={'a11y-hidden'}>{title}</h2>
             <button
               className={style.modalCloseButton}
-              onClick={() => setModalIsShow(false)}
+              onClick={() => dispatch(resetModal())}
             >
               <Image
                 src={modalCloseButton}
@@ -84,7 +74,7 @@ const Modal = ({
   );
 };
 
-export const getModalImage = (type: 'alert' | 'confirm' | 'prompt'): string => {
+export const getModalImage = (type: modalType): string => {
   if (type === 'alert') {
     return `${PUBLIC_IMAGE_PATH}/modal_success.png`;
   } else if (type === 'confirm') {
