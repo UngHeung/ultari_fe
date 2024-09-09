e69acf26b24f73002f47d7c291906fa0f990926c
ungheung <t01047377477@gmail.com>
2024-09-09 15:52:59 +0900

    refactor : nextjs Image lagacy layout 제거

--- src/components/common/layouts/Logo.tsx
+++ src/components/common/layouts/Logo.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { getImageClassAndSrc } from '../functions/getImageClassAndSrc';
+import style from '../styles/logo.module.css';
+import { getImageSrc } from '../functions/getImageSrc';
 
 export interface LogoOptions {
   id?: string;
@@ -9,16 +10,13 @@ export interface LogoOptions {
 }
 
 const Logo = ({ id, type }: LogoOptions) => {
-  const { className, src } = getImageClassAndSrc(type);
-
   return (
     <Link href={'/'}>
       <Image
         id={id}
-        className={className}
-        src={src}
+        className={style.logoImage}
+        src={getImageSrc(type)}
         alt={'울타리_로고 메인으로 이동'}
-        layout="responsive"
         placeholder="blur"
         priority
       />
