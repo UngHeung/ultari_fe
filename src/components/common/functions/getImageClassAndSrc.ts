56b28da1b02e6da35b590269b8c037aab7d99a94
ungheung <t01047377477@gmail.com>
2024-09-09 14:26:09 +0900

    refactor : 로고 이미지 크기 적용방식 변경 / 함수명 변경

--- src/components/common/functions/getImageClassAndSrc.ts
+++ src/components/common/functions/getImageClassAndSrc.ts
@@ -2,14 +2,9 @@ import basicLogo from '@/public/images/logo_basic.png';
 import smallLogo from '@/public/images/logo_small.png';
 import style from '../styles/logo.module.css';
 
-export const generateLogoImgOptions = (
-  type: 'basic' | 'small',
-  scale: number,
-) => {
+export const getImageClassAndSrc = (type: 'basic' | 'small') => {
   return {
-    src: type === 'basic' ? basicLogo : smallLogo,
     className: type === 'basic' ? style.basicLogo : style.smallLogo,
-    width: type === 'basic' ? 250 * (scale / 100) : 150 * (scale / 100),
-    height: 150 * (scale / 100),
+    src: type === 'basic' ? basicLogo : smallLogo,
   };
 };
