810ebec5014c3009731fad41e19e5b69e9744251
ungheung <t01047377477@gmail.com>
2024-09-09 23:06:44 +0900

    style : 불필요 코드 제거, env 변수명 변경

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -4,14 +4,12 @@ import style from './styles/button.module.css';
 import { useRouter } from 'next/navigation';
 import { handleSignUp } from './handlers/hendleSignUp';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { SliceOptions } from '../stores/constants/stateOptions';
+import { useDispatch } from 'react-redux';
 import { setModal } from '../stores/reducer/modalRducer';
 
 const SignUp = () => {
   const router = useRouter();
   const [disabled, setDisabled] = useState<boolean>(false);
-  const modal = useSelector((state: SliceOptions) => state.modal);
   const dispatch = useDispatch();
 
   return (
@@ -23,7 +21,6 @@ const SignUp = () => {
           setDisabled(true);
 
           const { status, success, message } = await handleSignUp(event);
-          console.log(success, message);
 
           dispatch(
             setModal({
--- src/components/common/constants/pathConst.ts
+++ src/components/common/constants/pathConst.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.DB_HOST;
+export const BASE_URL = process.env.NEXT_PUBLIC_DB_HOST;
 
 export const REISSUE_ACCESS = 'auth/reissue/access';
 export const REISSUE_REFRESH = 'auth/reissue/refresh';
