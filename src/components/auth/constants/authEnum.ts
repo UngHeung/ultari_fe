855e0e119085fd26061b4da36cf75bbeb306cf2a
ungheung <t01047377477@gmail.com>
2024-09-09 23:07:09 +0900

    refactor : auth enum 분리

---
+++ src/components/auth/constants/authEnum.ts
@@ -0,0 +1,13 @@
+export enum LoginOptionsEnum {
+  ACCOUNT = 'account',
+  PASSWORD = 'password',
+}
+
+export enum SignUpOptionsEnum {
+  ACCOUNT = 'account',
+  PASSWORD = 'password',
+  CHECK_PASSWORD = 'checkPassword',
+  NAME = 'name',
+  PHONE = 'phone',
+  EMAIL = 'email',
+}
