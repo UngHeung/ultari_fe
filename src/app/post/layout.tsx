349a8770e8446c8bbac2f6162cf1b00a3d7c9c7c
ungheung <t01047377477@gmail.com>
2024-09-11 00:56:42 +0900

    refactor : 게시물 페이지 유저 로그인 상태 확인하도록 변경

--- src/app/post/layout.tsx
+++ src/app/post/layout.tsx
@@ -2,11 +2,35 @@
 
 import Link from 'next/link';
 import style from './layout.module.css';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { SliceOptions } from '@/components/stores/constants/stateOptions';
 
 const PostLayout = ({ children }: { children: React.ReactNode }) => {
-  const router = usePathname();
-  const title = getTitle(router);
+  const pathname = usePathname();
+  const router = useRouter();
+  const isLoggedIn = useSelector(
+    (state: SliceOptions) => state.user?.isLoggedIn ?? false,
+  );
+  const [title, setTitle] = useState<string>('');
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push('/login');
+    } else {
+      const type = pathname.slice(1).split('/')[1];
+      if (type === 'list') {
+        setTitle('게시물 목록');
+      } else if (type === 'write') {
+        setTitle('게시물 작성');
+      } else if (type === 'update') {
+        setTitle('게시물 수정');
+      } else {
+        setTitle('게시물');
+      }
+    }
+  });
 
   return (
     <>
@@ -15,27 +39,12 @@ const PostLayout = ({ children }: { children: React.ReactNode }) => {
         {children}
       </section>
       <section className={style.buttonWrap}>
-        {title.endsWith('수정') && <Link href={'/post/update'}>수정</Link>}
         {!title.endsWith('목록') && <Link href={'/post/list'}>목록</Link>}
+        {title.endsWith('수정') && <Link href={'/post/update'}>수정</Link>}
         {!title.endsWith('작성') && <Link href={'/post/write'}>글쓰기</Link>}
       </section>
     </>
   );
-};
-
-export const getTitle = (router: string) => {
-  const type = router.slice(1).split('/')[1];
-  console.log(router);
-
-  if (type === 'list') {
-    return '게시물 목록';
-  } else if (type === 'write') {
-    return '게시물 작성';
-  } else if (type === 'update') {
-    return '게시물 수정';
-  } else {
-    return '게시물';
-  }
 };
 
 export default PostLayout;
