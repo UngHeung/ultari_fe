2d8047c8452e4111aebc7082e84469098805e6e1
ungheung <t01047377477@gmail.com>
2024-09-10 23:34:00 +0900

    update : store, slice interface user 추가

--- src/components/stores/configStore.ts
+++ src/components/stores/configStore.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modalRducer from './reducer/modalRducer';
 import postReducer from './reducer/postReducer';
+import userReducer from './reducer/userReducer';
 
 export const store = configureStore({
   reducer: {
     modal: modalRducer,
     post: postReducer,
+    user: userReducer,
   },
 });
