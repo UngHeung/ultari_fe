a5b2e96980567199f18980846abe025d3d086fec
ungheung <t01047377477@gmail.com>
2024-09-11 23:50:51 +0900

    remove : persist 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,23 @@
 'use client';
 
+import Layout from '@/components/common/Layout';
+import { store } from '@/components/stores/configStore';
+import { Provider } from 'react-redux';
 import './globals.css';
 import './reset.css';
-import { Provider } from 'react-redux';
-import { store } from '@/components/stores/configStore';
-import Layout from '@/components/common/Layout';
-import { PersistGate } from 'redux-persist/integration/react';
-import persistStore from 'redux-persist/es/persistStore';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const persistor = persistStore(store);
-
   return (
     <html lang="ko">
       <body>
         <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <Layout>
-              <main>{children}</main>
-            </Layout>
-          </PersistGate>
+          <Layout>
+            <main>{children}</main>
+          </Layout>
         </Provider>
       </body>
     </html>
