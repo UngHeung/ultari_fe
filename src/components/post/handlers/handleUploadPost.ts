a220a71eea1a83c532fed406159e1edcde3e2e07
ungheung <t01047377477@gmail.com>
2024-09-13 22:23:25 +0900

    refactor : post upload 함수 분리, showModal 함수 적용

---
+++ src/components/post/handlers/handleUploadPost.ts
@@ -0,0 +1,29 @@
+import { authAxios } from '@/apis/axiosAuth';
+import { BASE_URL } from '@/components/common/constants/pathConst';
+import { FormEvent } from 'react';
+
+export const handleUploadPost = async (event: FormEvent<HTMLFormElement>) => {
+  const formData = new FormData(event.currentTarget);
+  const title = formData.get('title');
+  const content = formData.get('content');
+
+  const data = { title, content };
+
+  const url = `${BASE_URL}/${'post'}`;
+  const response: any = await authAxios.post(url, data);
+
+  if (response.status === 201) {
+    return {
+      data: response.data,
+      status: response.status,
+      success: true,
+      message: '게시물 등록 성공!',
+    };
+  } else {
+    return {
+      status: response.status,
+      success: false,
+      message: response.message,
+    };
+  }
+};
