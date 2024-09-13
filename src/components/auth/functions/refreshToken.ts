7823607c2d19322945d77a66af77d025318449dc
ungheung <t01047377477@gmail.com>
2024-09-13 16:43:49 +0900

    rename : 파일 명 변경

---
+++ src/components/auth/functions/refreshToken.ts
@@ -0,0 +1,32 @@
+import {
+  reissueAccessToken,
+  reissueRefreshToken,
+} from '@/apis/functions/reissueToken';
+import { setAccessToken, setRefreshToken } from './tokenInteract';
+
+/**
+ * @param isAccess
+ * access token : true
+ * refresh token : false
+ */
+export const refreshToken = async (isAccess: boolean) => {
+  try {
+    if (isAccess) {
+      const newAccessToken = await reissueAccessToken();
+      setAccessToken(newAccessToken);
+    } else {
+      const newRefreshToken = await reissueRefreshToken();
+      setRefreshToken(newRefreshToken);
+    }
+
+    return {
+      success: true,
+      message: '새로운 토큰 발급이 완료되었습니다.',
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.response.data.message,
+    };
+  }
+};
