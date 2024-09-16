96f5818e3653822bf346e0cf183299559d127c4c
ungheung <t01047377477@gmail.com>
2024-09-17 00:38:44 +0900

    refactor : try catch문 추가

--- src/components/post/handlers/handleUploadPost.ts
+++ src/components/post/handlers/handleUploadPost.ts
@@ -6,24 +6,31 @@ export const handleUploadPost = async (event: FormEvent<HTMLFormElement>) => {
   const formData = new FormData(event.currentTarget);
   const title = formData.get('title');
   const content = formData.get('content');
-
   const data = { title, content };
+  const url = `${BASE_URL}/post`;
 
-  const url = `${BASE_URL}/${'post'}`;
-  const response: any = await authAxios.post(url, data);
+  try {
+    const response: any = await authAxios.post(url, data);
 
-  if (response.status === 201) {
+    if (response.status === 201) {
+      return {
+        data: response.data,
+        status: response.status,
+        success: true,
+        message: '게시물 등록 성공!',
+      };
+    } else {
+      return {
+        status: response.status,
+        success: false,
+        message: response.message,
+      };
+    }
+  } catch (error: any) {
     return {
-      data: response.data,
-      status: response.status,
-      success: true,
-      message: '게시물 등록 성공!',
-    };
-  } else {
-    return {
-      status: response.status,
+      status: error.status,
       success: false,
-      message: response.message,
+      message: error.response.data.message,
     };
   }
 };
