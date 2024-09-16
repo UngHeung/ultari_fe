a54328ca379c21ae4dc0409f4bab2cc6cb081a38
ungheung <t01047377477@gmail.com>
2024-09-16 18:13:09 +0900

    feat : post 정렬 상태 전역 관리로 변경

--- src/components/stores/reducer/PostListReducer.ts
+++ src/components/stores/reducer/PostListReducer.ts
@@ -1,6 +1,8 @@
 import { PostOptions } from '@/components/post/interfaces/postInterfaces';
 import { createSlice } from '@reduxjs/toolkit';
 
+export type OrderTypes = 'DESC' | 'ASC';
+
 export interface PostListOptions {
   asc: {
     list: PostOptions[];
@@ -13,6 +15,9 @@ export interface PostListOptions {
     count: number;
     lastIndex: number;
     next: string;
+  };
+  orderBy: {
+    type: OrderTypes;
   };
 }
 
@@ -29,6 +34,7 @@ const initialState: PostListOptions = {
     lastIndex: 0,
     next: '',
   },
+  orderBy: { type: 'DESC' },
 };
 
 export const postListSlice = createSlice({
@@ -41,10 +47,18 @@ export const postListSlice = createSlice({
     setPostListOrderByDesc: (state, actions) => {
       Object.assign(state.desc, actions.payload);
     },
+    setPostListOrderBy: (state, actions) => {
+      console.log(actions.payload);
+      Object.assign(state.orderBy, actions.payload);
+    },
     resetPostList: () => initialState,
   },
 });
 
-export const { setPostListOrderByAsc, setPostListOrderByDesc, resetPostList } =
-  postListSlice.actions;
+export const {
+  setPostListOrderByAsc,
+  setPostListOrderByDesc,
+  setPostListOrderBy,
+  resetPostList,
+} = postListSlice.actions;
 export default postListSlice.reducer;
