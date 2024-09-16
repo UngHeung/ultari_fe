9d6c83ee9ebc8b67b4e9684c71b5a002e4de2b94
ungheung <t01047377477@gmail.com>
2024-09-16 21:24:39 +0900

    feat : 찾기 페이지 메뉴 컴포넌트 추가

---
+++ src/components/auth/ForgotMenu.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+
+const ForgotMenu = () => {
+  return (
+    <>
+      <menu>
+        <ul>
+          <li>
+            <Link href={'/forgot/password'}>비밀번호 찾기</Link>
+          </li>
+          <li>
+            <Link href={'/forgot/account'}>아이디 찾기</Link>
+          </li>
+        </ul>
+      </menu>
+    </>
+  );
+};
+
+export default ForgotMenu;
