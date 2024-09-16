12fe4d6133c7e11b1e5c0b6007ae81534ba0a9ce
ungheung <t01047377477@gmail.com>
2024-09-16 18:29:55 +0900

    feat : 게시물 주제, 공개범위 반환 함수 추가

--- src/components/post/ListItem.tsx
+++ src/components/post/ListItem.tsx
@@ -25,8 +25,11 @@ const ListItem = (props: PostOptions, key: number) => {
 
           <section className={style.informationWrap}>
             <div className={style.typesWrap}>
-              <span className={style.visibility}>{props.visibility}</span>
-              <span className={style.type}>{props.type}</span>
+              <span className={style.visibility}>
+                {getVisibilityType(props.visibility)}
+              </span>
+              <> & </>
+              <span className={style.type}>{getPostType(props.type)}</span>
             </div>
 
             <div className={style.countsWrap}>
@@ -41,5 +44,29 @@ const ListItem = (props: PostOptions, key: number) => {
     </>
   );
 };
+
+function getVisibilityType(visibility: string) {
+  switch (visibility) {
+    case 'SCOPE_PUBLIC':
+      return '전체공개';
+    case 'SCOPE_TEAM':
+      return '목장공개';
+    case 'SCOPE_PERSONAL':
+      return '비공개';
+  }
+}
+
+function getPostType(type: string) {
+  switch (type) {
+    case 'TYPE_FREE':
+      return '자유';
+    case 'TYPE_SHARE':
+      return '나눔';
+    case 'TYPE_PRAYER':
+      return '기도';
+    case 'TYPE_THANKS':
+      return '감사';
+  }
+}
 
 export default ListItem;
