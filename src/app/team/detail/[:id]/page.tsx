345af0ec1c1f8e38de47e5f694a1cacf9bb32dd3
ungheung <t01047377477@gmail.com>
2024-09-27 00:47:04 +0900

    feat : team detail 페이지 추가

---
+++ src/app/team/detail/[:id]/page.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { authAxios } from '@/apis/axiosAuth';
+import { UserOptions } from '@/components/auth/interfaces/authInterface';
+import { BASE_URL } from '@/components/common/constants/pathConst';
+import { ParamsOptions } from '@/components/common/interfaces/paramsOptions';
+import { ModalState } from '@/components/stores/interfaces/stateInterface';
+import { setModal } from '@/components/stores/reducer/modalRducer';
+import TeamDetail from '@/components/team/TeamDetail';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+export interface TeamOptioins {
+  name: string;
+  community: string;
+  description?: string;
+  member: UserOptions[];
+  leader: UserOptions;
+  subLeader: UserOptions;
+  active: boolean;
+}
+
+const TeamMainPage = ({ params }: ParamsOptions) => {
+  const teamId = params[':id'];
+
+  const [teamData, setTeamData] = useState<TeamOptioins>();
+
+  const dispatch = useDispatch();
+
+  async function handleGetTeamById(teamId: number) {
+    const url = `${BASE_URL}/team/${teamId}`;
+
+    try {
+      const response = await authAxios.get(url);
+      return {
+        status: response.status,
+        success: true,
+        data: response.data,
+      };
+    } catch (error: any) {
+      return {
+        status: error.status,
+        success: false,
+        message: error.response.data.message,
+      };
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status, success, data, message } =
+        await handleGetTeamById(teamId);
+
+      if (success) {
+        setTeamData(data);
+      } else {
+        const modalData: ModalState = {
+          title: '서버 에러',
+          success,
+          type: 'alert',
+          routerType: 'back',
+          message: message!,
+          modalIsShow: true,
+        };
+
+        dispatch(setModal(modalData));
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <TeamDetail {...teamData!} />
+    </>
+  );
+};
+
+export default TeamMainPage;
