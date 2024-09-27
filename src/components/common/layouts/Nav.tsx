29e374892b83304871e9b5ffb288b82c6b21d4ab
ungheung <t01047377477@gmail.com>
2024-09-27 23:00:27 +0900

    update : 확인용 링크 변경

--- src/components/common/layouts/Nav.tsx
+++ src/components/common/layouts/Nav.tsx
@@ -11,18 +11,15 @@ const Nav = () => {
         {/* <li>
           <Link href={'/sign'}>회원가입</Link>
         </li> */}
+        {/* <li>
+          <Link href={'/forgot/password'}>아이디/비밀번호</Link>
+        </li> */}
         <li>
           <Link href={'/login'}>로그인</Link>
         </li>
         <li>
-          <Link href={'/forgot/password'}>아이디/비밀번호</Link>
-        </li>
-        <li>
           <Link href={'/post/list'}>게시판</Link>
         </li>
-        {/* <li>
-          <Link href={'/logout'}>로그아웃</Link>
-        </li> */}
         <li>
           <Link href={'/team/create'}>팀생성</Link>
         </li>
@@ -30,7 +27,7 @@ const Nav = () => {
           <Link href={'/team/list'}>팀목록</Link>
         </li>
         <li>
-          <Link href={'/team/detail/2'}>디테일</Link>
+          <Link href={'/logout'}>로그아웃</Link>
         </li>
       </ul>
     </nav>
