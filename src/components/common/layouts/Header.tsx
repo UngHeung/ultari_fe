59d79a889da57df63e5ba7b6d1e946dd89a012e3
ungheung <t01047377477@gmail.com>
2024-09-28 21:02:13 +0900

    fix : 헤더 로고 중복 링크 제거

--- src/components/common/layouts/Header.tsx
+++ src/components/common/layouts/Header.tsx
@@ -14,9 +14,7 @@ const Header = ({
     <header className={style.mainHeader}>
       <div className={style.headerWrap}>
         <section className={style.logoWrap}>
-          <Link href={'/'}>
-            <Logo type={'basic'} height={'100%'} />
-          </Link>
+          <Logo type={'basic'} height={'100%'} />
         </section>
         <section className={style.navWrap}>
           <Nav />
@@ -24,7 +22,7 @@ const Header = ({
         <section className={style.user}>
           {isLoggedIn ? (
             <div className={style.isLoggedIn}>
-              <span>{`${userName}님`}</span>
+              <Link href={`/user/my`}>{`${userName}님`}</Link>
               <div className={style.line}></div>
               <Link href={'/logout'}> 로그아웃</Link>
             </div>
