ab07aaa91d863ee736d3bee5112fd4b802acbeb1
ungheung <t01047377477@gmail.com>
2024-10-01 00:14:21 +0900

    update : 기존이미지가 있는 경우 기존이미지를 띄우도록 변경

--- src/components/user/ProfileUpload.tsx
+++ src/components/user/ProfileUpload.tsx
@@ -1,41 +1,45 @@
+import defaultProfile from '@/public/images/profile_default.png';
+import Image from 'next/image';
 import React, { ChangeEvent, SetStateAction, useState } from 'react';
 import ImageInput from '../post/elements/ImageInput';
 import style from './styles/update.module.css';
-import defaultProfile from '@/public/images/profile_default.png';
-import Image from 'next/image';
 
-const ProfileUpload = () => {
+const ProfileUpload = ({ currentProfile }: { currentProfile?: string }) => {
   const [selectedFile, setSelectedFile] = useState<File>();
   const [selectedImageBlob, setSelectedImageBlob] = useState<string>('');
 
   return (
-    <section className="imageWrap">
-      <section className="fileInputWrap">
-        <ImageInput
-          useLabel={true}
-          isMultiple={false}
-          labelStyleClass={style.selectButton}
-          labelValue={
-            <Image
-              src={selectedImageBlob ?? defaultProfile}
-              alt="프로필 기본"
-              width={50}
-              height={50}
-            />
-          }
-          name={'profile'}
-          id={'imageUpload'}
-          accept={['image/png', 'image/jpeg', 'image/jpg', 'image/gif']}
-          onChange={event => {
-            handleImageBlob(
-              event,
-              selectedFile!,
-              setSelectedFile,
-              setSelectedImageBlob,
-            );
-          }}
-        />
-      </section>
+    <section className={style.fileInputWrap}>
+      <ImageInput
+        useLabel={true}
+        isMultiple={false}
+        labelStyleClass={style.selectButton}
+        labelValue={
+          <Image
+            src={
+              selectedImageBlob
+                ? selectedImageBlob
+                : currentProfile
+                  ? currentProfile
+                  : defaultProfile
+            }
+            alt="프로필 기본"
+            width={50}
+            height={50}
+          />
+        }
+        name={'profile'}
+        id={'imageUpload'}
+        accept={['image/png', 'image/jpeg', 'image/jpg', 'image/gif']}
+        onChange={event => {
+          handleImageBlob(
+            event,
+            selectedFile!,
+            setSelectedFile,
+            setSelectedImageBlob,
+          );
+        }}
+      />
     </section>
   );
 };
