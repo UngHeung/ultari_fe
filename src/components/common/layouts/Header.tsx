250c9cdc2d28b1fd081116273a74c81c30ce37c2
ungheung <t01047377477@gmail.com>
2024-10-02 19:18:09 +0900

    update : 화면 크기에 따라 메뉴 타입을 변경 (일반 목록형, 숨김형)

--- src/components/common/layouts/Header.tsx
+++ src/components/common/layouts/Header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import style from '../styles/header.module.css';
 import Logo from './Logo';
 import Nav from './Nav';
+import { useState } from 'react';
 
 const Header = ({
   isLoggedIn,
@@ -10,32 +11,59 @@ const Header = ({
   isLoggedIn: boolean;
   userName: string;
 }) => {
+  const [isShow, setIsShow] = useState(false);
+
   return (
     <header className={style.mainHeader}>
       <div className={style.headerWrap}>
         <section className={style.logoWrap}>
-          <Logo type={'basic'} height={'100%'} />
+          <Logo type={'basic'} height={'100%'} setIsShow={setIsShow} />
         </section>
-        <section className={style.navWrap}>
-          <Nav />
-        </section>
-        <section className={style.user}>
-          {isLoggedIn ? (
-            <div className={style.isLoggedIn}>
-              <Link href={`/user/my`}>{`${userName}님`}</Link>
-              <div className={style.line}></div>
-              <Link href={'/logout'}> 로그아웃</Link>
-            </div>
-          ) : (
-            <div className={style.isLoggedOut}>
-              <Link href={'/login'}>로그인</Link>
-              <div className={style.line}></div>
-              <Link href={'/sign'}>회원가입</Link>
-              <div className={style.line}></div>
-              <Link href={'/forgot/password'}>계정 찾기</Link>
-            </div>
-          )}
-        </section>
+        <button
+          className={style.hamburger}
+          onClick={() => setIsShow(prev => !prev)}
+        >
+          메뉴
+        </button>
+        <div
+          className={`${style.mobileMenuWrap} ${isShow ? style.isShow : ''}`}
+        >
+          <section className={style.navWrap}>
+            <Nav setIsShow={setIsShow} />
+          </section>
+          <section className={style.user}>
+            {isLoggedIn ? (
+              <div className={style.isLoggedIn}>
+                <Link
+                  onClick={() => setIsShow(false)}
+                  href={`/user/my`}
+                >{`${userName}님`}</Link>
+                <div className={style.line}></div>
+                <Link onClick={() => setIsShow(false)} href={'/logout'}>
+                  {' '}
+                  로그아웃
+                </Link>
+              </div>
+            ) : (
+              <div className={style.isLoggedOut}>
+                <Link onClick={() => setIsShow(false)} href={'/login'}>
+                  로그인
+                </Link>
+                <div className={style.line}></div>
+                <Link onClick={() => setIsShow(false)} href={'/sign'}>
+                  회원가입
+                </Link>
+                <div className={style.line}></div>
+                <Link
+                  onClick={() => setIsShow(false)}
+                  href={'/forgot/password'}
+                >
+                  계정 찾기
+                </Link>
+              </div>
+            )}
+          </section>
+        </div>
       </div>
     </header>
   );
