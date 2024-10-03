f7a74c6ab01d0d48b7d9e4c19eaeef3b2bf4c74f
ungheung <t01047377477@gmail.com>
2024-10-04 01:13:37 +0900

    file : 미사용 파일 비활성화

--- src/components/common/layouts/Footer.tsx
+++ src/components/common/layouts/Footer.tsx
@@ -1,17 +1,17 @@
-import style from '../styles/footer.module.css';
-import Logo from './Logo';
+// import style from '../styles/footer.module.css';
+// import Logo from './Logo';
 
-const Footer = () => {
-  return (
-    <footer className={style.mainFooter}>
-      <div className={style.footerWrap}>
-        <section className={style.logoWrap}>
-          <Logo type={'small'} width={'50px'} />
-        </section>
-        <section className={style.contentWrap}>{'푸터입니다.'}</section>
-      </div>
-    </footer>
-  );
-};
+// const Footer = () => {
+//   return (
+//     <footer className={style.mainFooter}>
+//       <div className={style.footerWrap}>
+//         <section className={style.logoWrap}>
+//           <Logo type={'small'} width={'50px'} />
+//         </section>
+//         <section className={style.contentWrap}>{'푸터입니다.'}</section>
+//       </div>
+//     </footer>
+//   );
+// };
 
-export default Footer;
+// export default Footer;
