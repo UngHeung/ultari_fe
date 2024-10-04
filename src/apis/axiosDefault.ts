8ab71c5738936aa516522ed872102a012267867f
ungheung <t01047377477@gmail.com>
2024-10-04 17:07:53 +0900

    update : axios default 적용

--- src/apis/axiosDefault.ts
+++ src/apis/axiosDefault.ts
@@ -1,7 +1,7 @@
 import { BASE_URL } from '@/components/common/constants/pathConst';
 import axios from 'axios';
 
-export const defulatAxios = axios.create({
+export const defaultAxios = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
