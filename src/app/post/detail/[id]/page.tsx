f13eb390c03d5c99078bc7fedcaa96e806f8e651
ungheung <t01047377477@gmail.com>
2024-10-06 22:18:57 +0900

    rename : url detail로 구분

--- src/app/post/detail/[id]/page.tsx
+++ src/app/post/detail/[id]/page.tsx
@@ -5,7 +5,7 @@ import DetailLikeCount from '@/components/post/components/DetailLikeCount';
 import handleGetPost from '@/components/post/handlers/handleGetPost';
 import { PostState } from '@/components/stores/interfaces/stateInterface';
 import { useEffect, useState } from 'react';
-import Detail from '../../../components/post/Detail';
+import Detail from '../../../../components/post/Detail';
 
 const postPage = ({ params }: ParamsOptions) => {
   const postId = params['id'];
