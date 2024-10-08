d08424f40e604d91a3520c66d75938aad8cfc6fb
ungheung <t01047377477@gmail.com>
2024-10-08 20:06:44 +0900

    refactor : 게시물 목록 page에서 컴포넌트 분리

--- src/app/post/list/page.tsx
+++ src/app/post/list/page.tsx
@@ -1,103 +1,11 @@
 'use client';
 
-import ListMenu from '@/components/post/ListMenu';
-import PostList from '@/components/post/PostList';
-import fetchDataFromStoreOrServer from '@/components/post/functions/fetchDataFromStoreOrServer';
-import mapDispatchToProps from '@/components/post/functions/mapDispatchToProps';
-import moreFetchData from '@/components/post/functions/moreFetchData';
-import {
-  getPostListOptions,
-  PostOptions,
-} from '@/components/post/interfaces/postInterfaces';
-import { OrderTypes } from '@/components/stores/constants/stateOptions';
-import { SliceOptions } from '@/components/stores/interfaces/stateInterface';
-import { resetPostList } from '@/components/stores/reducer/PostListReducer';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import PostList from '@/components/post/components/PostList';
 
 const ListPage = () => {
-  const dispatch = useDispatch();
-
-  const listOrderByDesc = useSelector(
-    (state: SliceOptions) => state.postList.desc,
-  );
-  const listOrderByAsc = useSelector(
-    (state: SliceOptions) => state.postList.asc,
-  );
-  const listOrderBy = useSelector(
-    (state: SliceOptions) => state.postList.orderType,
-  );
-
-  const [nextPath, setNextPath] = useState<string>('');
-  const [postList, setPostList] = useState<PostOptions[]>([]);
-
-  useEffect(() => {
-    postListProcess(listOrderBy.value);
-
-    return () => {
-      dispatch(resetPostList());
-    };
-  }, []);
-
-  async function postListProcess(orderBy: OrderTypes) {
-    let postData: getPostListOptions = {
-      list: [],
-      count: 0,
-      next: '',
-    };
-
-    if (orderBy === 'DESC') {
-      postData = await fetchDataFromStoreOrServer(
-        orderBy,
-        listOrderByDesc,
-        'SCOPE_PUBLIC',
-      );
-
-      mapDispatchToProps.desc(dispatch, postData);
-    } else if (orderBy === 'ASC') {
-      postData = await fetchDataFromStoreOrServer(
-        orderBy,
-        listOrderByAsc,
-        'SCOPE_PUBLIC',
-      );
-
-      mapDispatchToProps.asc(dispatch, postData);
-    }
-
-    setPostList(postData.list);
-    setNextPath(postData.next);
-  }
-
   return (
     <>
-      <ListMenu postListProcess={postListProcess} />
-      <PostList postList={postList} />
-      {nextPath && (
-        <button
-          type={'button'}
-          onClick={async () => {
-            const orderBy = listOrderBy.value;
-            const postData = await moreFetchData(orderBy, nextPath);
-            const composeList = [...postList, ...postData.list!];
-            const dispatchData = {
-              list: composeList,
-              count: composeList.length,
-              next: postData.next ?? '',
-            };
-
-            setPostList(composeList);
-            setNextPath(postData.next!);
-
-            if (orderBy === 'DESC') {
-              mapDispatchToProps.desc(dispatch, dispatchData);
-            } else if (orderBy === 'ASC') {
-              mapDispatchToProps.asc(dispatch, dispatchData);
-            }
-          }}
-        >
-          더보기
-        </button>
-      )}
+      <PostList />
     </>
   );
 };
---
+++ src/components/post/components/PostList.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { OrderTypes } from '../../stores/constants/stateOptions';
+import { SliceOptions } from '../../stores/interfaces/stateInterface';
+import { resetPostList } from '../../stores/reducer/PostListReducer';
+import ListItem from './ListItem';
+import ListMenu from './ListMenu';
+import fetchDataFromStoreOrServer from '../functions/fetchDataFromStoreOrServer';
+import mapDispatchToProps from '../functions/mapDispatchToProps';
+import { getPostListOptions, PostOptions } from '../interfaces/postInterfaces';
+import PostListPaginate from './PostListPaginate';
+import style from './styles/list.module.css';
+
+const PostList = () => {
+  const dispatch = useDispatch();
+
+  const listOrderByDesc = useSelector(
+    (state: SliceOptions) => state.postList.desc,
+  );
+  const listOrderByAsc = useSelector(
+    (state: SliceOptions) => state.postList.asc,
+  );
+  const listOrderBy = useSelector(
+    (state: SliceOptions) => state.postList.orderType,
+  );
+
+  const [nextPath, setNextPath] = useState<string>('');
+  const [postList, setPostList] = useState<PostOptions[]>([]);
+
+  useEffect(() => {
+    postListProcess(listOrderBy.value);
+
+    return () => {
+      dispatch(resetPostList());
+    };
+  }, []);
+
+  async function postListProcess(orderBy: OrderTypes) {
+    let postData: getPostListOptions = {
+      list: [],
+      count: 0,
+      next: '',
+    };
+
+    if (orderBy === 'DESC') {
+      postData = await fetchDataFromStoreOrServer(
+        orderBy,
+        listOrderByDesc,
+        'SCOPE_PUBLIC',
+      );
+
+      mapDispatchToProps.desc(dispatch, postData);
+    } else if (orderBy === 'ASC') {
+      postData = await fetchDataFromStoreOrServer(
+        orderBy,
+        listOrderByAsc,
+        'SCOPE_PUBLIC',
+      );
+
+      mapDispatchToProps.asc(dispatch, postData);
+    }
+
+    setPostList(postData.list);
+    setNextPath(postData.next);
+  }
+
+  return (
+    <>
+      <ListMenu postListProcess={postListProcess} />
+
+      <ul className={style.postList}>
+        {postList ? (
+          postList.map((post: PostOptions, idx: number) => {
+            return <ListItem {...post} key={idx} />;
+          })
+        ) : (
+          <li>게시물이 없습니다.</li>
+        )}
+      </ul>
+      <PostListPaginate
+        dispatch={dispatch}
+        listOrderBy={listOrderBy}
+        nextPath={nextPath}
+        setNextPath={setNextPath}
+        postList={postList}
+        setPostList={setPostList}
+      />
+    </>
+  );
+};
+
+export default PostList;
