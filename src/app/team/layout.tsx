c8b9cd3588c85ed112e9403a04cf7c8325af02b7
ungheung <t01047377477@gmail.com>
2024-10-08 23:11:30 +0900

    update : 레이아웃 비로그인 접근 막지 않도록 변경

--- src/app/team/layout.tsx
+++ src/app/team/layout.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import { checkAuth } from '@/components/auth/functions/checkAuth';
-import {
-  ModalState,
-  SliceOptions,
-} from '@/components/stores/interfaces/stateInterface';
-import { setModal } from '@/components/stores/reducer/modalRducer';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import style from './layout.module.css';
 
 export type TeamPageType = 'list' | 'create';
@@ -18,31 +12,13 @@ const TeamLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const [title, setTitle] = useState<string>('');
 
-  const isLoggedIn = useSelector(
-    (state: SliceOptions) => state?.user?.isLoggedIn ?? false,
-  );
-
   useEffect(() => {
     const type: TeamPageType = pathname.slice(1).split('/')[1] as TeamPageType;
 
     setTitle(getTitle(type));
 
-    if (!checkAuth(type, isLoggedIn)) {
-      const modalData: ModalState = {
-        title: '권한 없음',
-        type: 'alert',
-        success: false,
-        message: '로그인 후 사용 가능합니다.',
-        modalIsShow: true,
-        routerType: 'push',
-        leftPath: '/login',
-      };
-
-      dispatch(setModal(modalData));
-    }
-
     return () => {};
-  }, [pathname, isLoggedIn]);
+  }, [pathname]);
 
   return (
     <>
