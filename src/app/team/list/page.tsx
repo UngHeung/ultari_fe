c023043678cf22c89c7219ed911718a80861bb92
ungheung <t01047377477@gmail.com>
2024-10-08 20:17:17 +0900

    refactor : page는 라우팅 역할에 집중하도록 컴포넌트 분리

--- src/app/team/list/page.tsx
+++ src/app/team/list/page.tsx
@@ -1,49 +1,31 @@
 'use client';
 
-import { authAxios } from '@/apis/axiosAuth';
+import { authAxios } from '@/apis/axiosInstance';
+import { makeResponseResult } from '@/components/common/functions/returnResponse';
 import { ModalState } from '@/components/stores/interfaces/stateInterface';
 import { setModal } from '@/components/stores/reducer/modalRducer';
 import TeamList from '@/components/team/TeamList';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TeamOptioins } from '../detail/[id]/page';
-import axios from 'axios';
 
 const TeamListPage = () => {
   const [teamList, setTeamList] = useState<TeamOptioins[]>([]);
   const dispatch = useDispatch();
 
-  async function handleGetTeamList() {
-    const url = `/team`;
-
+  async function teamListProcess() {
     try {
-      const response = await authAxios.get(url);
+      const response = await authAxios.get('/team');
 
-      return {
-        status: response.status,
-        success: true,
-        data: response.data,
-      };
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        return {
-          status: error.status,
-          success: false,
-          message: error.response?.data.message || '서버에 문제 발생',
-        };
-      } else {
-        return {
-          status: 500,
-          success: false,
-          message: '서버에 문제 발생',
-        };
-      }
+      return makeResponseResult(response);
+    } catch (error: any) {
+      return makeResponseResult(error);
     }
   }
 
   useEffect(() => {
     (async () => {
-      const { success, data, message } = await handleGetTeamList();
+      const { success, data, message } = await teamListProcess();
       if (success) {
         setTeamList([...(data || [])]);
       } else {
