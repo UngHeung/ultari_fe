f77d606241c0ce4b21eb64439febfdd27c6e28aa
ungheung <t01047377477@gmail.com>
2024-10-08 23:41:07 +0900

    refactor : 멤버목록 컴포넌트화 및 분리

---
+++ src/components/team/components/MemberList.tsx
@@ -0,0 +1,34 @@
+import defaultProfile from '@/public/images/profile_default.png';
+import Image from 'next/image';
+import style from '../styles/teamDetail.module.css';
+import { UserOptions } from '../../auth/interfaces/authInterface';
+
+const MemberList = ({ member }: { member?: UserOptions[] }) => {
+  return (
+    <ul className={style.memberList}>
+      {member ? (
+        member.map((member, idx) => {
+          return (
+            <li key={idx}>
+              <div className={style.profileWrap}>
+                <span className={style.memberProfile}>
+                  <Image
+                    src={member?.profile ?? defaultProfile}
+                    alt={'목장원_프로필'}
+                    width={25}
+                    height={25}
+                  />
+                </span>
+                <span className={style.memberName}>{member.name}</span>
+              </div>
+            </li>
+          );
+        })
+      ) : (
+        <li>{'목장원이 없습니다.'}</li>
+      )}
+    </ul>
+  );
+};
+
+export default MemberList;
