3a457c50d684f7758062c5d2600b4c41d82c891a
ungheung <t01047377477@gmail.com>
2024-10-08 19:53:33 +0900

    rename : 파일 이동, 주석 추가

--- src/apis/reissueToken.ts
+++ src/apis/reissueToken.ts
@@ -2,17 +2,23 @@ import {
   REISSUE_ACCESS,
   REISSUE_REFRESH,
 } from '@/components/common/constants/pathConst';
-import { refreshAxios } from '../axiosReissue';
+import { refreshAxios } from './axiosInstance';
 
 const accessReissueUrl = REISSUE_ACCESS;
 const refreshReissueUrl = REISSUE_REFRESH;
 
+/**
+ * Reissue access token
+ */
 export const reissueAccessToken = async () => {
   const response = await refreshAxios.post(accessReissueUrl);
   const accessToken = response.data.accessToken;
   return accessToken;
 };
 
+/**
+ * Reissue refresh token
+ */
 export const reissueRefreshToken = async () => {
   const response = await refreshAxios.post(refreshReissueUrl);
   const refreshToken = response.data.refreshToken;
