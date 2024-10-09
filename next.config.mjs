ac18b7ec94912ebbf5ed5cf8103698d2775b8214
ungheung <t01047377477@gmail.com>
2024-10-09 17:08:15 +0900

    feat : env 접근용 db 상수 생성 및 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,23 @@
 /** @type {import('next').NextConfig} */
+const protocol = process.env.NEXT_PUBLIC_DB_PROTOCOL;
+const hostname = process.env.NEXT_PUBLIC_DB_HOST;
+const port = process.env.NEXT_PUBLIC_DB_PORT;
+
 const nextConfig = {
   reactStrictMode: false,
   images: {remotePatterns: [{
-    protocol: 'http',
-    hostname: 'localhost',
-    port: '3000',
+    protocol,
+    hostname,
+    port,
     pathname: '/public/**'
   }]},
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_DB_HOST + '/api/:path*'
+        destination:
+          `${protocol}://${hostname}:${port}` +
+          '/api/:path*',
       },
     ];
   },
