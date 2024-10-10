50c733e59b659f3e357fea7c8b688b5ec310efbf
ungheung <t01047377477@gmail.com>
2024-10-10 15:54:08 +0900

    feat : cors update

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,4 +1,3 @@
-import { BASE_URL } from '@/components/common/constants/dbConst';
 import axios from 'axios';
 import {
   callbackRequestConfig,
@@ -11,14 +10,14 @@ import {
  * 권한이 필요없는 Axios 인스턴스
  */
 export const baseAxios = axios.create({
-  baseURL: BASE_URL,
+  baseURL: '/api',
 });
 
 /**
  * 권한이 필요한 Axios 인스턴스
  */
 export const authAxios = axios.create({
-  baseURL: BASE_URL,
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -38,7 +37,7 @@ authAxios.interceptors.response.use(
  * 권한이 필요한 파일 업로드용 Axios
  */
 export const fileUploadAxios = axios.create({
-  baseURL: BASE_URL,
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
   },
@@ -58,8 +57,7 @@ fileUploadAxios.interceptors.response.use(
  * 토큰 재발급용 Axios
  */
 export const refreshAxios = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
+  baseURL: '/api',
 });
 
 refreshAxios.interceptors.request.use(
