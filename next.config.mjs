643810a00ae526ca18a8002bc19d2d3b564c6aff
ungheung <t01047377477@gmail.com>
2024-10-10 22:40:49 +0900

    test: base url test

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/api/:path*',
+        source: 'https://ultari.xyz/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: 'https://api-ultari.xyz'},
@@ -26,7 +26,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: 'https://ultari.xyz/api/:path*',
         destination: process.env.NEXT_PUBLIC_DB_HOST + '/api/:path*',
       },
     ];
