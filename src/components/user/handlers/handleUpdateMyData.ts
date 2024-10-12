7e024cb6af097eab592568fdba1c27cfa7bfcf36
ungheung <t01047377477@gmail.com>
2024-10-13 00:37:06 +0900

    feat : 유저 프로필 업데이트기능 추가

--- src/components/user/handlers/handleUpdateMyData.ts
+++ src/components/user/handlers/handleUpdateMyData.ts
@@ -5,30 +5,30 @@ import { FormEvent } from 'react';
 async function handleUpdateMyData(event: FormEvent<HTMLFormElement>) {
   const formData = new FormData(event.currentTarget);
 
-  const file: File = formData.get('profile') as File;
   const data = {
-    profile: '',
+    path: '',
     phone: formData.get('phone') as string,
     email: formData.get('email') as string,
     community: formData.get('community') as string,
   };
 
-  if (file.name) {
-    try {
-      const response = await fileUploadAxios.post('/common/image', {
-        image: file,
-      });
+  const file: File = formData.get('profile') as File;
 
-      data.profile = response.data;
-    } catch (error: any) {
-      return makeResponseResult(error);
-    }
+  const imageFormData = new FormData();
+  imageFormData.append('image', file);
+
+  try {
+    const response = await fileUploadAxios.post('/common/image', imageFormData);
+
+    data.path = response.data.fileName;
+  } catch (error: any) {
+    return makeResponseResult(error);
   }
 
   try {
     const response = await authAxios.patch(`/user`, data);
 
-    return makeResponseResult(response);
+    return makeResponseResult(response, '내정보수정');
   } catch (error: any) {
     return makeResponseResult(error);
   }
