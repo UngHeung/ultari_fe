3ddeeab91bdc1ea5137822ca1d451ef9ff0ed1ef
ungheung <t01047377477@gmail.com>
2024-10-16 10:55:34 +0900

    feat : 유저 인증 기능 추가

---
+++ src/components/common/functions/userAuthentication.ts
@@ -0,0 +1,28 @@
+import { ModalState } from '@/components/stores/interfaces/stateInterface';
+import { setModal } from '@/components/stores/reducer/modalRducer';
+import { Dispatch } from '@reduxjs/toolkit';
+import { MouseEvent } from 'react';
+
+function userAuthentication(
+  isLoggedIn: boolean,
+  dispatch: Dispatch,
+  event: MouseEvent<HTMLAnchorElement>,
+) {
+  if (!isLoggedIn) {
+    event.preventDefault();
+
+    const ModalData: ModalState = {
+      title: '로그인',
+      type: 'prompt',
+      message: `로그인이 필요한 페이지입니다.\n확인시 로그인 페이지로 이동합니다.`,
+      success: false,
+      routerType: 'push',
+      modalIsShow: true,
+      leftPath: '/login',
+    };
+
+    dispatch(setModal(ModalData));
+  }
+}
+
+export default userAuthentication;
