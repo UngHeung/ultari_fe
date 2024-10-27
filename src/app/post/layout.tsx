9dda9e2ba593830ee90323ee8d3beff39a3f7237
ungheung <t01047377477@gmail.com>
2024-10-27 16:39:33 +0900

    refactor : 제목 대신 포지션 상태값으로 위치 확인

--- src/app/post/layout.tsx
+++ src/app/post/layout.tsx
@@ -1,91 +1,62 @@
 'use client';
 
 import userAuthentication from '@/components/common/functions/userAuthentication';
-import {
-  ModalState,
-  SliceOptions,
-} from '@/components/stores/interfaces/stateInterface';
-import { setModal } from '@/components/stores/reducer/modalRducer';
+import { SliceOptions } from '@/components/stores/interfaces/stateInterface';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import style from './layout.module.css';
 
-export type PostPageType = 'list' | 'write' | 'update' | 'detail' | 'delete';
+export type PostPagePosition =
+  | 'list'
+  | 'write'
+  | 'update'
+  | 'detail'
+  | 'delete';
 
 const PostLayout = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch();
   const pathname = usePathname();
+
   const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [position, setPosition] = useState<PostPagePosition>('list');
 
   const isLoggedIn = useSelector(
     (state: SliceOptions) => state.logged.isLoggedIn,
   );
-  const userId = useSelector((state: SliceOptions) => state.user.id);
-  const postId = useSelector((state: SliceOptions) => state.post.id);
-  const postAuthorId = useSelector(
-    (state: SliceOptions) => state.post.author?.id,
-  );
 
   useEffect(() => {
-    const type: PostPageType = pathname.slice(1).split('/')[1] as PostPageType;
+    const type: PostPagePosition = pathname
+      .slice(1)
+      .split('/')[1] as PostPagePosition;
 
     if (type !== 'list' && type !== 'detail') {
       userAuthentication(isLoggedIn, dispatch);
     }
 
-    if (type === 'list') {
-      setTitle('게시물 목록');
-    } else if (type === 'write') {
-      setTitle('게시물 작성');
-    } else if (type === 'update') {
-      setTitle('게시물 수정');
-    } else {
-      setTitle('게시물');
-    }
+    setPosition(type);
+    setTitle('자유 게시판');
+    setDescription('자유롭게 소통해요.');
   }, [pathname, isLoggedIn]);
 
   return (
     <>
       <section className={style.postWrap}>
-        <h2 className={style.postTitle}>{title}</h2>
+        <section className={style.head}>
+          <h2 className={style.title}>{title}</h2>
+          <span className={style.description}>{description}</span>
+        </section>
         {children}
       </section>
       <section className={style.buttonWrap}>
-        {!title.endsWith('목록') && <Link href={'/post/list'}>목록</Link>}
-        {title.endsWith('게시물') && postAuthorId === userId && isLoggedIn && (
-          <>
-            <Link href={`/post/update/${postId}`}>수정</Link>
-            <Link
-              href={'/'}
-              onClick={async event => {
-                event.preventDefault();
-
-                userAuthentication(isLoggedIn, dispatch, event);
-
-                const modalData: ModalState = {
-                  title: '삭제 확인',
-                  type: 'prompt',
-                  message: '정말 삭제하시겠습니까?',
-                  modalIsShow: true,
-                  routerType: 'replace',
-                  leftPath: `/post/delete/${postId}`,
-                };
-
-                dispatch(setModal(modalData));
-              }}
-            >
-              삭제
-            </Link>
-          </>
-        )}
-        {!title.endsWith('작성') && (
+        {!position.includes('write') && (
           <Link
             href={'/post/write'}
             onClick={event => userAuthentication(isLoggedIn, dispatch, event)}
           >
-            글쓰기
+            {writeButton}
           </Link>
         )}
       </section>
@@ -94,3 +65,31 @@ const PostLayout = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default PostLayout;
+
+const writeButton = (
+  <svg
+    width="50"
+    height="50"
+    viewBox="0 0 50 50"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="8.33334"
+      y="22.9167"
+      width="33.3333"
+      height="4.16667"
+      rx="2.08333"
+      fill="#767676"
+    />
+    <rect
+      x="27.0833"
+      y="8.33337"
+      width="33.3333"
+      height="4.16667"
+      rx="2.08333"
+      transform="rotate(90 27.0833 8.33337)"
+      fill="#767676"
+    />
+  </svg>
+);
