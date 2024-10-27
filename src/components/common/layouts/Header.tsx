eeca0f1b7855d7464f1664d3fbcc85d492ca28b5
ungheung <t01047377477@gmail.com>
2024-10-27 20:49:04 +0900

    style : 헤더 스타일 변경

--- src/components/common/layouts/Header.tsx
+++ src/components/common/layouts/Header.tsx
@@ -1,72 +1,75 @@
+import UserProfile from '@/components/user/components/UserProfile';
 import Link from 'next/link';
 import { useState } from 'react';
 import style from '../styles/header.module.css';
-import Logo from './Logo';
 import Nav from './Nav';
 
 const Header = ({
   isLoggedIn,
-  userName,
+  profile,
 }: {
   isLoggedIn: boolean;
-  userName: string;
+  profile: string;
 }) => {
   const [isShow, setIsShow] = useState(false);
 
   return (
     <header className={style.mainHeader}>
       <div className={style.headerWrap}>
-        <section className={style.logoWrap}>
-          <Logo type={'basic'} height={'100%'} setIsShow={setIsShow} />
+        <section className={style.buttonWrap}>
+          <button
+            className={style.hamburger}
+            onClick={() => setIsShow(prev => !prev)}
+          >
+            {hamburger}
+          </button>
         </section>
-        <button
-          className={style.hamburger}
-          onClick={() => setIsShow(prev => !prev)}
-        >
-          메뉴
-        </button>
-        <div
-          className={`${style.mobileMenuWrap} ${isShow ? style.isShow : ''}`}
-        >
-          <section className={style.navWrap}>
-            <Nav setIsShow={setIsShow} isLoggedIn={isLoggedIn} />
-          </section>
-          <section className={style.user}>
-            {isLoggedIn ? (
-              <div className={style.isLoggedIn}>
-                <Link
-                  onClick={() => setIsShow(false)}
-                  href={`/user/my`}
-                >{`${userName}님`}</Link>
-                <div className={style.line}></div>
-                <Link onClick={() => setIsShow(false)} href={'/logout'}>
-                  {' '}
-                  로그아웃
-                </Link>
-              </div>
-            ) : (
-              <div className={style.isLoggedOut}>
-                <Link onClick={() => setIsShow(false)} href={'/login'}>
-                  로그인
-                </Link>
-                <div className={style.line}></div>
-                <Link onClick={() => setIsShow(false)} href={'/sign'}>
-                  회원가입
-                </Link>
-                <div className={style.line}></div>
-                <Link
-                  onClick={() => setIsShow(false)}
-                  href={'/forgot/password'}
-                >
-                  계정 찾기
-                </Link>
-              </div>
-            )}
-          </section>
-        </div>
+        <section className={style.profileWrap}>
+          {isLoggedIn ? (
+            <Link onClick={() => setIsShow(false)} href={`/user/my`}>
+              <UserProfile path={profile} size={30} />
+            </Link>
+          ) : (
+            <Link onClick={() => setIsShow(false)} href={'/login'}>
+              로그인
+            </Link>
+          )}
+        </section>
+      </div>
+
+      <div className={`${style.mobileMenuWrap} ${isShow ? style.isShow : ''}`}>
+        <section className={style.navWrap}>
+          <Nav setIsShow={setIsShow} isLoggedIn={isLoggedIn} />
+        </section>
+        <section className={style.user}>
+          <div className={style.isLoggedOut}>
+            <div className={style.line}></div>
+            <Link onClick={() => setIsShow(false)} href={'/sign'}>
+              회원가입
+            </Link>
+            <div className={style.line}></div>
+            <Link onClick={() => setIsShow(false)} href={'/forgot/password'}>
+              계정 찾기
+            </Link>
+          </div>
+        </section>
       </div>
     </header>
   );
 };
 
 export default Header;
+
+const hamburger = (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="2" y="5" width="20" height="2" rx="1" fill="white" />
+    <rect x="2" y="11" width="20" height="2" rx="1" fill="white" />
+    <rect x="2" y="17" width="20" height="2" rx="1" fill="white" />
+  </svg>
+);
