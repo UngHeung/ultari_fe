7ed1df8ee98a186cde5f84265502a4a6dcd729c1
ungheung <t01047377477@gmail.com>
2024-10-28 23:11:09 +0900

    feat : 이미지 업로드 안내 문자열 추가, 스타일 변경

--- src/components/post/components/ImageUploadForm.tsx
+++ src/components/post/components/ImageUploadForm.tsx
@@ -47,14 +47,14 @@ const ImageUploadForm = ({
 
   return (
     <>
-      <form onSubmit={imageUploadProcess}>
+      <form onSubmit={imageUploadProcess} className={style.imageUploadForm}>
         <section className={style.imageWrap}>
           <section className={style.fileInputWrap}>
             <ImageInput
               useLabel={true}
               isMultiple={true}
               labelStyleClass={style.selectButton}
-              labelValue={addImageButtonIcon}
+              labelValue={addImageIcon}
               name={'images'}
               id={'imageUpload'}
               accept={['image/png', 'image/jpeg', 'image/jpg', 'image/gif']}
@@ -78,18 +78,43 @@ const ImageUploadForm = ({
           />
           <section className={'fileUploadButtonWrap'}></section>
           <section>
-            {selectedFiles && selectedFiles.length > 0 ? (
-              <SelectedImageConfirmButton
-                type={'submit'}
-                onClick={() => setConfirmedImages(prev => !prev)}
-                confirmedImages={confirmedImages}
-                disabled={uploadDisabled}
-              />
-            ) : (
-              <p className={style.descriptionImageUpload}>
-                {'⬅︎ 이미지를 추가하려면 여기를 눌러주세요.'}
+            <SelectedImageConfirmButton
+              type={'submit'}
+              onClick={() => setConfirmedImages(prev => !prev)}
+              confirmedImages={confirmedImages}
+              disabled={uploadDisabled}
+            />
+            <div
+              style={{
+                display:
+                  !selectedFiles || selectedFiles?.length <= 0
+                    ? 'inline-block'
+                    : 'none',
+              }}
+              className={style.descWrap}
+            >
+              <p className={style.description}>
+                <span>{'이 곳을 눌러 업로드할 이미지를 선택해주세요.'}</span>
+                <span>{'확장자 - JPG / JPEG / PNG / GIF / WEBP.'}</span>
               </p>
-            )}
+              <p className={style.description}>
+                <span>{'이미지 업로드는 최대 3개까지 가능하며.'}</span>
+                <span>{'업로드 가능한 용량은 최대 2Mb 입니다.'}</span>
+              </p>
+            </div>
+
+            <p
+              className={`${style.description} ${style.last}`}
+              style={{
+                top: !selectedFiles || selectedFiles?.length <= 0 ? 0 : 10,
+              }}
+            >
+              <span>{'글을 저장하기 전'}</span>
+              <strong className={style.uploadMini}>{uploadIconForDesc}</strong>
+              <span>{'버튼을 눌러'}</span>
+              <strong className={style.warnMini}>{warnIcon}</strong>
+              <span>{'선택한 이미지들을 먼저 업로드해주세요.'}</span>
+            </p>
           </section>
         </section>
       </form>
@@ -99,17 +124,54 @@ const ImageUploadForm = ({
 
 export default ImageUploadForm;
 
-export const addImageButtonIcon = (
+const addImageIcon = (
   <svg
-    width="20"
-    height="18"
-    viewBox="0 0 20 18"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M17.5 2.125C17.8438 2.125 18.125 2.40625 18.125 2.75V15.2422L17.9297 14.9883L12.6172 8.11328C12.4414 7.88281 12.1641 7.75 11.875 7.75C11.5859 7.75 11.3125 7.88281 11.1328 8.11328L7.89062 12.3086L6.69922 10.6406C6.52344 10.3945 6.24219 10.25 5.9375 10.25C5.63281 10.25 5.35156 10.3945 5.17578 10.6445L2.05078 15.0195L1.875 15.2617V15.25V2.75C1.875 2.40625 2.15625 2.125 2.5 2.125H17.5ZM2.5 0.25C1.12109 0.25 0 1.37109 0 2.75V15.25C0 16.6289 1.12109 17.75 2.5 17.75H17.5C18.8789 17.75 20 16.6289 20 15.25V2.75C20 1.37109 18.8789 0.25 17.5 0.25H2.5ZM5.625 7.75C5.87123 7.75 6.11505 7.7015 6.34253 7.60727C6.57002 7.51305 6.77672 7.37494 6.95083 7.20083C7.12494 7.02672 7.26305 6.82002 7.35727 6.59253C7.4515 6.36505 7.5 6.12123 7.5 5.875C7.5 5.62877 7.4515 5.38495 7.35727 5.15747C7.26305 4.92998 7.12494 4.72328 6.95083 4.54917C6.77672 4.37506 6.57002 4.23695 6.34253 4.14273C6.11505 4.0485 5.87123 4 5.625 4C5.37877 4 5.13495 4.0485 4.90747 4.14273C4.67998 4.23695 4.47328 4.37506 4.29917 4.54917C4.12506 4.72328 3.98695 4.92998 3.89273 5.15747C3.7985 5.38495 3.75 5.62877 3.75 5.875C3.75 6.12123 3.7985 6.36505 3.89273 6.59253C3.98695 6.82002 4.12506 7.02672 4.29917 7.20083C4.47328 7.37494 4.67998 7.51305 4.90747 7.60727C5.13495 7.7015 5.37877 7.75 5.625 7.75Z"
-      fill="var(--color-placeholder)"
+      d="M20 5C21.1 5 22 5.9 22 7V19C22 20.1 21.1 21 20 21H4C2.9 21 2 20.1 2 19V7C2 5.9 2.9 5 4 5H7.17L9 3H15L16.83 5H20ZM20 19V7H4V19H20ZM14 12L11 15.72L9 13L6 17H18L14 12Z"
+      fill="#767676"
     />
+  </svg>
+);
+
+const uploadIconForDesc = (
+  <svg
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6.9999 9.50249C6.80157 9.50249 6.63544 9.43529 6.5015 9.30089C6.3671 9.16696 6.2999 9.00083 6.2999 8.80249V3.79749L4.9874 5.10999C4.8474 5.24999 4.68407 5.31999 4.4974 5.31999C4.31074 5.31999 4.14157 5.24416 3.9899 5.09249C3.8499 4.95249 3.78294 4.78613 3.789 4.59339C3.7946 4.40113 3.86157 4.24083 3.9899 4.11249L6.5099 1.59249C6.5799 1.52249 6.65574 1.47279 6.7374 1.44339C6.81907 1.41446 6.90657 1.39999 6.9999 1.39999C7.09324 1.39999 7.18074 1.41446 7.2624 1.44339C7.34407 1.47279 7.4199 1.52249 7.4899 1.59249L10.0099 4.11249C10.1499 4.25249 10.2169 4.41863 10.2108 4.61089C10.2052 4.80363 10.1382 4.96416 10.0099 5.09249C9.8699 5.23249 9.70377 5.30529 9.5115 5.31089C9.31877 5.31696 9.1524 5.24999 9.0124 5.10999L7.6999 3.79749V8.80249C7.6999 9.00083 7.63294 9.16696 7.499 9.30089C7.3646 9.43529 7.19824 9.50249 6.9999 9.50249ZM2.7999 12.3025C2.4149 12.3025 2.08544 12.1655 1.8115 11.8916C1.5371 11.6172 1.3999 11.2875 1.3999 10.9025V9.50249C1.3999 9.30416 1.46687 9.13779 1.6008 9.00339C1.7352 8.86946 1.90157 8.80249 2.0999 8.80249C2.29824 8.80249 2.4646 8.86946 2.599 9.00339C2.73294 9.13779 2.7999 9.30416 2.7999 9.50249V10.9025H11.1999V9.50249C11.1999 9.30416 11.2671 9.13779 11.4015 9.00339C11.5354 8.86946 11.7016 8.80249 11.8999 8.80249C12.0982 8.80249 12.2644 8.86946 12.3983 9.00339C12.5327 9.13779 12.5999 9.30416 12.5999 9.50249V10.9025C12.5999 11.2875 12.4629 11.6172 12.189 11.8916C11.9146 12.1655 11.5849 12.3025 11.1999 12.3025H2.7999Z"
+      fill="#2D8738"
+    />
+  </svg>
+);
+
+const warnIcon = (
+  <svg
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_6_1344)">
+      <path
+        d="M6.41675 8.74999H7.58341V9.91666H6.41675V8.74999ZM6.41675 4.08332H7.58341V7.58332H6.41675V4.08332ZM6.99425 1.16666C3.77425 1.16666 1.16675 3.77999 1.16675 6.99999C1.16675 10.22 3.77425 12.8333 6.99425 12.8333C10.2201 12.8333 12.8334 10.22 12.8334 6.99999C12.8334 3.77999 10.2201 1.16666 6.99425 1.16666ZM7.00008 11.6667C4.42175 11.6667 2.33341 9.57832 2.33341 6.99999C2.33341 4.42166 4.42175 2.33332 7.00008 2.33332C9.57841 2.33332 11.6667 4.42166 11.6667 6.99999C11.6667 9.57832 9.57841 11.6667 7.00008 11.6667Z"
+        fill="#C80E0E"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_6_1344">
+        <rect width="14" height="14" fill="white" />
+      </clipPath>
+    </defs>
   </svg>
 );
