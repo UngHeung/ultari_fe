85704a4403f95024228720863274fd06f892e459
ungheung <t01047377477@gmail.com>
2024-10-30 11:35:56 +0900

    update : 레이아웃에 검색 추가

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,7 +1,7 @@
 import { reissueAccessToken, reissueRefreshToken } from '@/apis/reissueToken';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { useRouter } from 'next/navigation';
-import React, { Dispatch, useEffect } from 'react';
+import React, { Dispatch, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import getUserDataFromToken from '../auth/functions/getUserDataFromToken';
 import {
@@ -9,6 +9,7 @@ import {
   setRefreshToken,
 } from '../auth/functions/tokenInteract';
 import Modal from '../modal/Modal';
+import SearchForm from '../post/components/SearchForm';
 import { SliceOptions } from '../stores/interfaces/stateInterface';
 import { setUser } from '../stores/reducer/userReducer';
 import Footer from './layouts/Footer';
@@ -17,14 +18,16 @@ import Header from './layouts/Header';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch();
   const router = useRouter();
+
   const modalIsShow = useSelector(
     (state: SliceOptions) => state.modal?.modalIsShow,
   );
-
   const isLoggedIn: boolean = useSelector(
     (state: SliceOptions) => state.logged.isLoggedIn,
   );
   const profile = useSelector((state: SliceOptions) => state.user.path);
+
+  const [isSearching, setIsSearching] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
@@ -36,9 +39,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <>
       <Header isLoggedIn={isLoggedIn} profile={profile ?? ''} />
       {children}
-      <Footer />
+      <Footer setIsSearching={setIsSearching} />
       {/* <Footer /> */}
       {modalIsShow && <Modal />}
+      {isSearching && <SearchForm setIsSearching={setIsSearching} />}
     </>
   );
 };
