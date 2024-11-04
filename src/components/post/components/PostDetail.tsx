cb134cde47fe740e2f2aa5bd872283cb6d2d9de7
ungheung <t01047377477@gmail.com>
2024-11-04 21:03:16 +0900

    refactor : 페이지 분리

--- src/components/post/components/PostDetail.tsx
+++ src/components/post/components/PostDetail.tsx
@@ -1,34 +1,59 @@
-// import Comment from '@/components/comments/components/Comment';
-// import usePostStore, { PostStore } from '@/components/stores/post/postStore';
-// import { useEffect } from 'react';
-// import handleGetPost from '../handlers/handleGetPost';
-// import style from '../styles/detail.module.css';
-// import DetailContent from './DetailContent';
-// import DetailLikeCount from './DetailLikeCount';
+'use server';
 
-// const PostDetail = ({ postId }: { postId: number }) => {
-//   const setPost = usePostStore((state: PostStore) => state.setPost);
-//   const post = usePostStore((state: PostStore) => state.post);
+import { baseAxios } from '@/apis/axiosInstance';
+import Comment from '@/components/comments/components/Comment';
+import ImagesSlider from '@/components/common/components/ImagesSlider';
+import DetailLikeCount from '@/components/post/components/DetailLikeCount';
+import mapContentType from '@/components/post/functions/mapContentType';
+import mapVisibility from '@/components/post/functions/mapVisibility';
+import { PostOptions } from '@/components/post/interfaces/postInterfaces';
+import style from '@/components/post/styles/detail.module.css';
+import UserProfile from '@/components/user/components/UserProfile';
+import { revalidatePath } from 'next/cache';
 
-//   useEffect(() => {
-//     (async () => {
-//       const { success, data } = await handleGetPost(postId);
+const PostDetail = async ({ postId }: { postId: number }) => {
+  const url = `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_DB_HOST}/api/post/${postId}/detail`;
 
-//       if (success && post) {
-//         setPost(data);
-//       }
-//     })();
-//   }, []);
+  revalidatePath(`/post/detail/${postId}`, 'page');
 
-//   return (
-//     <div className={style.detailWrap}>
-//       {post && <DetailContent postData={post} />}
-//       {post && <DetailLikeCount postData={post} />}
-//       {post && post.comments && (
-//         <Comment comments={post.comments} targetId={postId} />
-//       )}
-//     </div>
-//   );
-// };
+  try {
+    const response = await baseAxios.get(url);
 
-// export default PostDetail;
+    const postData: PostOptions = response.data;
+
+    return (
+      <section className={style.detailWrap}>
+        <section className={style.head}>
+          <h2 className={style.title}>{postData.title}</h2>
+          <span className={style.type}>
+            {`#${mapContentType(postData.contentType)}`}
+          </span>
+          <span className={style.visibility}>
+            {`#${mapVisibility(postData.visibility)}`}
+          </span>
+          <div className={style.authorWrap}>
+            <span>
+              <UserProfile path={postData.author.profile?.path} size={25} />
+            </span>
+            <span>{postData.author.name}</span>
+          </div>
+        </section>
+
+        <section className={style.main}>
+          {postData.images && postData.images?.length > 0 && (
+            <ImagesSlider folder={'post'} images={postData.images} />
+          )}
+          <pre className={style.content}>{postData?.content}</pre>
+        </section>
+        {postData && <DetailLikeCount postData={postData} />}
+        <Comment comments={postData.comments} targetId={postId} />
+      </section>
+    );
+  } catch (error: any) {
+    <section>
+      <h2>{'Not Found 404'}</h2>
+    </section>;
+  }
+};
+
+export default PostDetail;
