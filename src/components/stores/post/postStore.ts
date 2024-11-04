ddc4fb630075d5ec7c164dcd1a1cc2267c72dca4
ungheung <t01047377477@gmail.com>
2024-11-04 13:56:46 +0900

    feat : comment 상태 추가, 수정, 삭제 추가

--- src/components/stores/post/postStore.ts
+++ src/components/stores/post/postStore.ts
@@ -1,4 +1,7 @@
-import { PostOptions } from '@/components/post/interfaces/postInterfaces';
+import {
+  CommentOptions,
+  PostOptions,
+} from '@/components/post/interfaces/postInterfaces';
 import { create } from 'zustand';
 
 const initialState: PostOptions = {
@@ -28,12 +31,41 @@ export interface PostStore {
   post: PostOptions;
   setPost: (post: PostOptions) => void;
   resetPost: () => void;
+
+  addComment: (comment: CommentOptions) => void;
+  updateComment: (id: number, comment: CommentOptions) => void;
+  deleteComment: (id: number) => void;
 }
 
 const usePostStore = create<PostStore>(set => ({
   post: initialState,
   setPost: (post: PostOptions) => set({ post }),
   resetPost: () => set({ post: initialState }),
+
+  addComment: (comment: CommentOptions) => {
+    set(state => ({
+      post: {
+        ...state.post,
+        comments: [comment, ...(state.post.comments ?? [])],
+      },
+    }));
+  },
+  updateComment: (id: number, comment: CommentOptions) =>
+    set(state => ({
+      post: {
+        ...state.post,
+        comments: state.post.comments?.map(item =>
+          item.id === id ? comment : item,
+        ),
+      },
+    })),
+  deleteComment: (id: number) =>
+    set(state => ({
+      post: {
+        ...state.post,
+        comments: state.post.comments?.filter(item => item.id !== id),
+      },
+    })),
 }));
 
 export default usePostStore;
